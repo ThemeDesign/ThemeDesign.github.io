ced37b9e829ca1672dc13b29533b2dc2c94c37fe
Hector <hectoruch1@hotmail.com>
2015-09-19 12:26:10 +0200

    .
    .

---
+++ theme6/js/web.js
@@ -0,0 +1,9 @@
+$(window).resize(function () {
+	$("header").css("height", $(window).height() + "px");
+});
+
+$(document).ready(function () {
+	$("header").css("height", $(window).height() + "px");
+});
+
+$('#h1first').fitText();
