84cc03033eddf66b2692377919cc607f6a403752
Hector <hectoruch1@hotmail.com>
2015-09-20 16:01:16 +0200

    .
    .

--- theme6/js/web.js
+++
@@ -1,7 +0,0 @@
-$(window).resize(function () {
-	$("header").css("height", $(window).height() + "px");
-});
-
-$(document).ready(function () {
-	$("header").css("height", $(window).height() + "px");
-});
